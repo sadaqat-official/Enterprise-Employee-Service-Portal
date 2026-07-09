0e756785d03d127ad8be715424948b062a5a1f3e
Sadaquat Razzaq <srazzaq@splonline.com.sa>
2026-07-09 14:38:30 +0500

    Setup applicaiton and infrastructure service registeration

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Application;
+
+namespace Api.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(AssemblyReference).Assembly);
+        });
+
+        return services;
+    }
+}
---
+++ API/Extensions/InfrastructureServiceExtensions.cs
@@ -0,0 +1,9 @@
+namespace Api.Extensions;
+
+public static class InfrastructureServiceExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
