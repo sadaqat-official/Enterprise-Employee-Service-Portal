9cfada5e3cc0be90f51d278f91a44aa20c7651a8
Sadaquat Razzaq <srazzaq@splonline.com.sa>
2026-07-09 16:56:51 +0500

    Add employee feature foundation

---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,24 @@
+﻿
+using Domain.Enums;
+namespace Domain.Entities;
+
+public class Employee
+{
+    public Guid Id { get; set; }
+
+    public string EmployeeNumber { get; set; } = string.Empty;
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string JobTitle { get; set; } = string.Empty;
+
+    public string Department { get; set; } = string.Empty;
+
+    public EmployeeStatus Status { get; set; } = EmployeeStatus.Active;
+
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+}
---
+++ Domain/Enums/EmployeeStatus.cs
@@ -0,0 +1,7 @@
+namespace Domain.Enums;
+
+public enum EmployeeStatus
+{
+    Active = 1,
+    Inactive = 2
+}
